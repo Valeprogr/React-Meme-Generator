a2345ee3a74d0ddef7faebfd9133edb8337cc695
valeviit <valeviit@icloud.com>
2022-11-07 12:17:06 +0100

    add img, bootstrap, and some style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import {useState, useEffect} from "react";
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 function App() {
   const [meme, getMeme]= useState();
   const url = 'https://api.imgflip.com/get_memes';
   
-
-
 useEffect(()=>{
-getMemes();
-},[])
-
   const getMemes = () => {
     axios.get(url)
     .then((response)=>{
@@ -21,14 +18,31 @@ getMemes();
     })
     .catch(error =>console.error (`Error: ${error}`));
   }
-  //   let mappingMeme = 
-  // console.log(meme.data.memes)
-  return (
-  <div>
-    <h1>Hello World!</h1>
-    <div className='memePhoto'>
 
-    </div>
+
+getMemes();
+},[])
+
+  
+let randomNum = Math.floor(Math.random() * 100);
+
+//  let mappingMeme = meme.data.memes.map((meme)=>
+//   <img src={meme.url} alt='meme'/> )
+
+  return (
+  <div className='container-fluid'>
+    <h1 className='text-center'>Meme generator!</h1>
+    <div className='memePhoto card'>
+    { meme ? <img className='img-fluid' src={meme.data.memes[0].url} alt='meme'/>
+    
+    :
+    <p>Error 404</p>
+
+    }
+    <h5 class="card-title">Create your Meme</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <button className='btn btn-info'>Edit</button>
+   </div>
   </div>
   )
 }
